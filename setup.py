4bcb6e6681de52f51bb1c15508c564ee5dcdeef2
Clay McClure <clay@daemons.net>
2013-08-08 15:34:17 -0400

    Support `python setup.py test`

--- setup.py
+++ setup.py
@@ -1,4 +1,18 @@
+import sys
+
 from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
 
 setup(
     name = "pystogram",
@@ -19,4 +33,5 @@ setup(
     packages = ['pystogram'],
     scripts = ['bin/pystogram'],
     tests_require = ['pytest'],
+    cmdclass = {'test': PyTest},
 )
